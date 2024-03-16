f101159c1484bef93eeb3cf5ceec733df64fb87b
anton.a <anton.a@itfox-web.com>
2024-03-16 16:41:08 +0600

    Initial Commit

---
+++ e1_app/e1_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  e1_appApp.swift
+//  e1_app
+//
+//  Created by Антон Адамсон on 16.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct e1_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
